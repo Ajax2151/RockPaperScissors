fd117833fee36e6bbbd43d4cc3ccfa07c38b7970
Ajax <ajackson2511@gmail.com>
2024-08-14 17:50:15 -0500

    Added scorekeeping functionality for computer and player rounds. Created basic player loop for a "first to three" playstyle.
    Added final score readout and exit message.

--- Program.cs
+++ Program.cs
@@ -1,72 +1,100 @@
 ﻿string userChoice = "";
 int userToss = 0;
+int playerScore = 0;
+int computerScore = 0;
 
 string? readResult;
+// bool playGame = true;
 
-Console.WriteLine("Please enter your choice: Rock, Paper, or Scissors.");
-readResult = Console.ReadLine();
-if (readResult != null)
+do
 {
-    userChoice = readResult.ToLower().Trim();
-}
-
-if ((userChoice != "rock") && (userChoice != "paper") && (userChoice != "scissors"))
-{
-    Console.WriteLine("Sorry, that's not a valid option.");
-}
-else
-{
-    switch (userChoice)
+    Console.WriteLine("Please enter your choice: Rock, Paper, or Scissors.");
+    readResult = Console.ReadLine();
+    if (readResult != null)
     {
-        case "rock":
-            userToss = 0;
-            break;
-        case "paper":
-            userToss = 1;
-            break;
-        case "scissors":
-            userToss = 2;
-            break;
+        userChoice = readResult.ToLower().Trim();
     }
 
-    switch (userToss)
+    if ((userChoice != "rock") && (userChoice != "paper") && (userChoice != "scissors"))
     {
-        case 0:
-            Console.WriteLine("You chose 'Rock'.");
-            break;
-        case 1:
-            Console.WriteLine("You chose 'Paper'.");
-            break;
-        case 2:
-            Console.WriteLine("You chose 'Scissors'.");
-            break;
+        Console.WriteLine("Sorry, that's not a valid option.");
     }
-
-    int computerToss = 0;
-    Random toss = new Random();
-    computerToss = toss.Next(3);
-
-    switch (computerToss)
-    {
-        case 0:
-            Console.WriteLine("The computer chose 'Rock'.");
-            break;
-        case 1:
-            Console.WriteLine("The computer chose 'Paper'.");
-            break;
-        case 2:
-            Console.WriteLine("The computer chose 'Scissors'.");
-            break;
-    }
-
-    if (userToss == computerToss)
-        Console.WriteLine("It's a draw!");
-    else if ((userToss == 0) && (computerToss == 2))
-        Console.WriteLine("Congratulations - you win!");
-    else if ((computerToss == 0) && (userToss == 2))
-        Console.WriteLine("Sorry, you lose.");
-    else if (userToss > computerToss)
-        Console.WriteLine("Congratulations - you win!");
     else
-        Console.WriteLine("Sorry, you lose.");
-}
+    {
+        switch (userChoice)
+        {
+            case "rock":
+                userToss = 0;
+                break;
+            case "paper":
+                userToss = 1;
+                break;
+            case "scissors":
+                userToss = 2;
+                break;
+        }
+
+        switch (userToss)
+        {
+            case 0:
+                Console.WriteLine("You chose 'Rock'.");
+                break;
+            case 1:
+                Console.WriteLine("You chose 'Paper'.");
+                break;
+            case 2:
+                Console.WriteLine("You chose 'Scissors'.");
+                break;
+        }
+
+        int computerToss = 0;
+        Random toss = new Random();
+        computerToss = toss.Next(3);
+
+        switch (computerToss)
+        {
+            case 0:
+                Console.WriteLine("The computer chose 'Rock'.");
+                break;
+            case 1:
+                Console.WriteLine("The computer chose 'Paper'.");
+                break;
+            case 2:
+                Console.WriteLine("The computer chose 'Scissors'.");
+                break;
+        }
+
+        if (userToss == computerToss)
+        {
+            Console.WriteLine("It's a draw!");
+        }
+        else if ((userToss == 0) && (computerToss == 2))
+        {
+            playerScore++;
+            Console.WriteLine("Congratulations - you win!");
+        }
+        else if ((computerToss == 0) && (userToss == 2))
+        {
+            computerScore++;
+            Console.WriteLine("Sorry, you lose.");
+        }
+        else if (userToss > computerToss)
+        {
+            playerScore++;
+            Console.WriteLine("Congratulations - you win!");
+        }
+        else
+        {
+            computerScore++;
+            Console.WriteLine("Sorry, you lose.");
+        }
+        
+        Console.WriteLine($"You've won {playerScore} rounds. The computer has won {computerScore} rounds.");
+    }
+} while (playerScore < 3 && computerScore < 3);
+
+Console.WriteLine($"The final score is: \n\tPlayer: {playerScore}\n\tComputer: {computerScore}");
+Console.WriteLine("Thank you for playing!");
+
+Console.WriteLine("Press Enter to exit.");
+Console.ReadLine();
